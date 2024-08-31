25e562fc6d921fa428683c749a8d45d1ac521ae0
omi4858 <omi4858@gmail.com>
2024-08-31 19:30:54 +0530

    variables

--- 09-specail-vars.sh
+++ 09-specail-vars.sh
@@ -4,3 +4,5 @@ echo "all vairables are passed to the scrpit: $@"
 echo "No of vairables are passed to the scrpit: $#"
 echo "script name: $0"
 echo "curret working dirctory: $PWD"
+echo "Home directly of current user: $HOME"
+
