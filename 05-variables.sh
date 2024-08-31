a49b635aede68a0ad6a183c3c121043f009cca95
omi4858 <omi4858@gmail.com>
2024-08-31 16:58:28 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,12 @@
 
 echo "Please enter ur username"
 
-read USERNAME
+read -s USERNAME #takes input into USERNAME varialbe
 
 echo "username  entered is $USERNAME"
+
+echo "Please enter your password"
+
+read -s PASSWORD
+echo "Passsowrd entered is : $PASSWORD"
+
