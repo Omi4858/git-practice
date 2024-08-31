28585b0471a16b5e41d45c2a500fe5071a10d5fe
omi4858 <omi4858@gmail.com>
2024-08-31 16:45:29 +0530

    variables

---
+++ 04-varialbes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1.How are you doing ?"
+echo "$PERSON1 : Good to hear, I am learnig Devops"
+echo "$PERSON2: Nice, all the best $PERSON1"
