ac6c407a2ed63b20f4691d2d57af1d82c4fbe0af
omi4858 <omi4858@gmail.com>
2024-09-01 14:19:04 +0530

    functions

---
+++ 12-functions.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+#echo "User id is: $USERID "
+
+VALIDATE(){
+    echo "exit stauts : $1"
+
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Plese run this script with root priviliges"
+    exit 1
+    
+    fi
+
+    dnf list installed git
+    
+    VALIDATE $?
+
+    if [ $? -ne 0 ]
+    then
+        echo " Git is not installed, going to install it... "
+        dnf install git -y
+
+    else
+        echo "Get is already installed, nothing to to"
+
+    fi
+
+    dnf list installed mysql
+
+    if [ $? -ne 0 ]
+
+    then
+        echo "My sql is not installed....going to install"
+        dnf install mysql -y
+        if [ $? -ne 0 ]
+        then
+        echo "Mysql installation is failur..please check"
+        exit 1
+        else 
+        echo "Mysql installation is success"
+        fi
+        else
+        echo "Mysql is already installed..nothing to do"
+        fi
